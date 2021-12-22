1d2dfa0a30d25277be0268feddac28a68450836e
kravamango <kravamango@gmail.com>
2021-12-22 23:15:36 +0300

    new

---
+++ 50.js
@@ -0,0 +1,20 @@
+function nextInLine(arr, item) {
+    // Only change code below this line
+    
+    arr.push(item);
+  
+  var temp = arr.shift();
+  
+  return temp; // Change this line
+  }
+    // Only change code above this line
+  
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+  
